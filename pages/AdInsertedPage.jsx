6a3a65463fd71ebced0544fd463a662511259ae3
Martin Sedmihradský <100826013+sedmima20@users.noreply.github.com>
2024-01-09 09:29:59 +0100

    Upraven text potvrzující odeslání inzerátu
    Zdůrazněna nutnost přihlášení stejným Google účtem, pokud chce uživatel upravit inzerát.

--- pages/AdInsertedPage.jsx
+++ pages/AdInsertedPage.jsx
@@ -12,7 +12,7 @@ export default function AdInsertedPage() {
         <>
             <section>
                 <h1>AI seznamka Zuzana - Vložení inzerátu</h1>
-                <div className="ok-message-box">Inzerát byl úspěšně odeslán! Do 24 hodin od jeho odeslání ho můžete libovolně upravovat. Poté se tato možnost zamkne a inzerát bude zaktivován. Děkujeme, že jste se rozhodli využít naši službu AI seznamka Zuzana! ✨</div>
+                <div className="ok-message-box">Inzerát byl úspěšně odeslán! Do 24 hodin od jeho odeslání ho můžete libovolně upravovat (po přihlášení stejným Google účtem). Poté se tato možnost zamkne a inzerát bude zaktivován. Děkujeme, že jste se rozhodli využít naši službu AI seznamka Zuzana! ✨</div>
                 <button onClick={handleClick}>Upravit inzerát</button>
             </section>
         </>
